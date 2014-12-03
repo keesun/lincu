84ff39a552300c208094fd4995ab9d53733e0a42
Keesun Baik <keesun.baik@navercorp.com>
2014-12-03 18:51:38 +0900

    setup application
    close #1
    * setup maven project
    * added spring boot
    * added stub controller for '/'

---
+++ src/main/java/io/lincu/Application.java
@@ -0,0 +1,18 @@
+package io.lincu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Keeun Baik
+ */
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
