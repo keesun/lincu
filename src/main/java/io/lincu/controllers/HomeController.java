5ed673d58ec7479784c656d9bfe71fd671aa2fe3
Keesun Baik <keesun.baik@navercorp.com>
2014-12-15 16:41:34 +0900

    making list

--- src/main/java/io/lincu/controllers/HomeController.java
+++ src/main/java/io/lincu/controllers/HomeController.java
@@ -1,16 +1,7 @@
 package io.lincu.controllers;
 
-import io.lincu.domains.Account;
-import io.lincu.domains.Content;
-import io.lincu.repositories.AccountRepository;
-import io.lincu.repositories.ContentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 /**
  * @author Keeun Baik
